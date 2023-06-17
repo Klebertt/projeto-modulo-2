8415913c9ace69ed09e5ad481d46ff5d650c8c67
Klebertt <126410502+Klebertt@users.noreply.github.com>
2023-06-17 10:15:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-document.getElementById('botao').addEventListener('click', getData);
+document.getElementById('botao1').addEventListener('click', getData);
 
 function getData() {
 
-    fetch('https://randomuser.me/api/?results=15')
+    fetch('https://randomuser.me/api/?results=1')
         .then(res => res.json())
         .then(data => {
 
 
             let author = data.results;
-            let output = "";
+            let output = "<h2><center>Get User Data</center></h2>";
 
             author.forEach(function (lists) {
-                
-                
-                
-                output +=`
-
-                <tr>
-
-                    <td>${lists.name.first}</td>
-                    <td>${lists.cell}</td>
-                    <td>${lists.dob.date}</td>
-                    <td>${lists.dob.age}</td>
-                    <td>${lists.email}</td>
-                    <td>${lists.gender}</td>
-                    <td>${lists.location.city}</td>
-                    <td>${lists.location.country}</td>
-                    <td>${lists.location.postcode}</td>
-                </tr>
-
-                `;
-
+                output += `
+                <div class="container">
+                    <div class="card mt-4 bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h2>Nome: ${lists.name.first}</h2></li>
+                            <li id="foto" class="list-group-item"><img src="${lists.picture.large}"></li>
+                            <li class="list-group-item">Phone Number: ${lists.cell}</li>
+                            <li class="list-group-item">DOB: ${lists.dob.date}</li>
+                            <li class="list-group-item">Age: ${lists.dob.age}</li>
+                            <li class="list-group-item">Email ID: ${lists.email}</li>
+                            <li class="list-group-item">Gender: ${lists.gender}</li>
+                            <li class="list-group-item">City: ${lists.location.city}</li>
+                            <li class="list-group-item">Country: ${lists.location.country}</li>
+                            <li class="list-group-item">PostCode: ${lists.location.postcode}</li>
+                        </ul>
+                    </div>
+                </div> `;
             });
 
             document.getElementById('output').innerHTML = output;
 
         });
 };
+
