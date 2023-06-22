8577cf7709dbafe483322de725da096e458dcf2a
Klebertt <126410502+Klebertt@users.noreply.github.com>
2023-06-21 22:41:34 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+describe('Fetch API Tests', () => {
+  it('getData deve retornar os dados da API', async () => {
+    const data = await getData();
+    assert.ok(data); 
+    assert.ok(data.length > 0); 
+  });
+
+  it('tab deve retornar os dados da API da pagina 1', async () => {
+    const data = await tab();
+    assert.ok(data); //
+    assert.ok(data.length > 0);
+  });
+
+  it('tab1 deve retornar os dados da API da pagina 2', async () => {
+    const data = await tab1();
+    assert.ok(data);
+    assert.ok(data.length > 0);
+  });
+
+  it('tab2 deve retornar os dados da API da pagina 3', async () => {
+    const data = await tab2();
+    assert.ok(data);
+    assert.ok(data.length > 0);
+  });
+
+});
